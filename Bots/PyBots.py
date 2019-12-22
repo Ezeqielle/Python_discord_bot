db810caee789d23adeda787fdcc0a99a8ecb0f3c
Mr Wolf <mathis.dimascio@gmail.com>
2019-12-22 20:05:49 +0100

    purge ok but can be opti

--- Bots/PyBots.py
+++ Bots/PyBots.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import time
+import asyncio
 
 def read_token():
     with open("token.txt", "r") as f:
@@ -31,11 +32,17 @@ async def roll(ctx, dice: str):
     await ctx.send(result)
 
 @bot.command()
+async def purge(ctx):
+    async for msg in ctx.channel.history(limit = None):
+        await msg.delete()
+
+@bot.command()
 async def help(ctx):
     help_list = discord.Embed(colour = discord.Colour.red())
     help_list.set_author(name = "Command Helper")
     help_list.add_field(name = "__**>help**__", value = "All commands and how use them", inline = False)
     help_list.add_field(name = "__**>roll**__", value = ">roll NdN | done N roll between 1 and N", inline = False)
+    help_list.add_field(name = "__**>purge**__", value = "Delete all messages in the channel where the command was send (require admin role)", inline = False)
     help_list.add_field(name = "__**>othercommand**__", value = "other explaination", inline = False)
     
     await ctx.send(embed = help_list)
