ea72c5f81fd5857a3a557f3d13e4e1a5972a355a
Mr Wolf <mathis.dimascio@gmail.com>
2019-12-22 18:05:57 +0100

    add help command

--- Bots/PyBots.py
+++ Bots/PyBots.py
@@ -11,6 +11,7 @@ def read_token():
 token = read_token()
 
 bot = commands.Bot(command_prefix = '>')
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -19,7 +20,7 @@ async def on_ready():
     print(bot.user.id)
     print('----------')
 
-@ bot.command()
+@bot.command()
 async def roll(ctx, dice: str):
     try:
         rolls, limit = map(int, dice.split('d'))
@@ -29,6 +30,15 @@ async def roll(ctx, dice: str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
+@bot.command()
+async def help(ctx):
+    help_list = discord.Embed(colour = discord.Colour.red())
+    help_list.set_author(name = "Command Helper")
+    help_list.add_field(name = "__**>help**__", value = "All commands and how use them", inline = False)
+    help_list.add_field(name = "__**>roll**__", value = ">roll NdN | done N roll between 1 and N", inline = False)
+    help_list.add_field(name = "__**>othercommand**__", value = "other explaination", inline = False)
+    
+    await ctx.send(embed = help_list)
 
 #last line
 bot.run(token)
