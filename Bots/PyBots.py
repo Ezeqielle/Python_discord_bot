195ff2848e915afe713dc2990a6c59d5b4a99c4f
Mr Wolf <mathis.dimascio@gmail.com>
2019-12-22 17:01:59 +0100

    start new project old project go trash

--- Bots/PyBots.py
+++ Bots/PyBots.py
@@ -1,8 +1,7 @@
 import discord
+from discord.ext import commands
+import random
 import time
-import asyncio
-
-messages = joined = 0
 
 def read_token():
     with open("token.txt", "r") as f:
@@ -10,40 +9,26 @@ def read_token():
         return lines[0].strip()
 
 token = read_token()
-client = discord.Client()
 
-#quand un user rejoint le serveur
-@client.event
-async def on_member_join(member):
-    global joined
-    joined += 1
-    for channel in member.server.channels:
-        if str(channel) == "welcome":
-            ""
-            #await client.send_message(f"""welcome to the server {member.mention}""")
+bot = commands.Bot(command_prefix = '>')
 
-#a la reception des commandes
-@client.event
-async def on_message(message):
-    global messages
-    messages += 1
-    id = client.get_guild(496660517218418688)
-    channels = ["testbot"]
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('----------')
 
-    if message.content == ">help":
-        help = discord.Embed(title = "Help Bot", description = "Quelques commandes utiles")
-        help.add_field(name = ">hello", value = "greets the user")
-        help.add_field(name = ">users", value = "prints numbers of users")
-        await message.channel.send(content = None, embed = help)
-
-    if str(message.channel) in channels:
-        if message.content.find(">hello") != -1:
-            await message.channel.send("Hi")
-        elif message.content == ">users":
-            await message.channel.send(f"""# of Members: {id.member_count}""")
-    else:
-        print(f"""User: {message.author} tried to do command {message.content}, in channel {message.channel}""")
+@ bot.command()
+async def roll(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
 
-#last lines
-client.run(token)
+#last line
+bot.run(token)
