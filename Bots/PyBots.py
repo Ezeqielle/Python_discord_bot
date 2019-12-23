81c59d15911b97fd91dfdd0c86f2143b2978efd6
Mr Wolf <mathis.dimascio@gmail.com>
2019-12-23 15:21:16 +0100

    add admin role for purge commande and name for roll

--- Bots/PyBots.py
+++ Bots/PyBots.py
@@ -29,12 +29,18 @@ async def roll(ctx, dice: str):
         await ctx.send('Format has to be in NdN!')
         return
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+    author = ctx.author.name
+    await ctx.send(author+' rolled a **'+result+'**')
 
 @bot.command()
+@commands.has_role('admin')
 async def purge(ctx):
     async for msg in ctx.channel.history(limit = None):
         await msg.delete()
+@purge.error
+async def purge_error(ctx, error):
+    if isinstance(error, commands.errors.MissingRole): 
+        await ctx.send('> **Can\'t do that! You don\'t have admin role. Please ask an admin to send command or give you admin role.**')
 
 @bot.command()
 async def duplicate(ctx):
@@ -46,7 +52,7 @@ async def help(ctx):
     help_list = discord.Embed(colour = discord.Colour.red())
     help_list.set_author(name = "Command Helper")
     help_list.add_field(name = "__**>help**__", value = "Display all commands and how use them", inline = False)
-    help_list.add_field(name = "__**>roll**__", value = ">roll NdN | done N roll between 1 and N", inline = False)
+    help_list.add_field(name = "__**>roll NdN**__", value = "Done N roll between 1 and N", inline = False)
     help_list.add_field(name = "__**>purge**__", value = "Delete all messages in the channel where command was sent (require admin role)", inline = False)
     help_list.add_field(name = "__**>duplicate**__", value = "Duplicate the channel where command was sent", inline = False)
     
